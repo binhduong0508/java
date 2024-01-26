6d6c5c195e5b9ee3f962c1c4f22ea2c18823be06
binhduong0508 <binhduong05082004@gmail.com>
2024-01-26 11:16:22 +0700

    trang30lab2

--- baithuchanh/trang30.java
+++ baithuchanh/trang30.java
@@ -2,18 +2,13 @@ import java.util.Scanner;
 
 public class trang30 {
     public static void main(String[] args) {
-        int n,sum;
+        int n,sum=0,du;
         Scanner sc= new Scanner(System.in);
         System.out.print("nhap n:");n=sc.nextInt();
-        sum = 0;
-        if(n<10)
-            sum = sum +n;
-        else {
-         for(int i=1;i<=n;i++){
-       
-          sum = sum + n % 10;
-          n= n / 10;
-         }
+        while(n>0){
+            du= n % 10;
+            n= n / 10;
+           sum+= du;
         }
         System.out.print("tong la:"+ sum );
     }
---
+++ baithuchanh/trang47.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class trang47 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        do{
+            System.out.
+        }
+    }
+}
