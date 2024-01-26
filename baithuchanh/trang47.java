908b14264fc4afe805d839555baf5345f42b0d72
binhduong0508 <binhduong05082004@gmail.com>
2024-01-26 16:58:46 +0700

    trang47lab2

--- baithuchanh/trang47.java
+++ baithuchanh/trang47.java
@@ -1,10 +1,16 @@
-import java.util.Scanner;
+
+    import java.util.Scanner;
 
 public class trang47 {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        do{
-            System.out.
-        }
+       int n, sum=0;
+       do {
+        System.out.print("nhap so nguyen bat ky: ");
+        n = sc.nextInt();
+        sum = sum + n;
+       } while (sum<100);
+       System.out.printf("tong la: "+ sum);
     }
 }
+
