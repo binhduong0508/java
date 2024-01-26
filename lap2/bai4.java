59bdb3b13817dd74ffffaa2817a1d365169c2d2a
binhduong0508 <binhduong05082004@gmail.com>
2024-01-26 20:04:14 +0700

    bai4lab2

---
+++ lap2/bai4.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class bai4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.print("nhap 1 so nguyen: ");
+        n = sc.nextInt();
+        switch(n){
+            case 0:
+            System.out.print("thang 1");
+            break;
+            case 1:
+            System.out.print("thang 2");
+            break;
+            case 2:
+            System.out.print("thang 3");
+            break;
+            case 3:
+            System.out.print("thang 4");
+            break;
+            case 4:
+            System.out.print("thang 5");
+            break;
+            case 5:
+            System.out.print("thang 6");
+            break;
+            case 6:
+            System.out.print("thang 7");
+            break;
+            case 7:
+            System.out.print("thang 8");
+            break;
+            case 8:
+            System.out.print("thang 9");
+            break;
+            case 9:
+            System.out.print("thang 10");
+            break;
+            case 10:
+            System.out.print("thang 11");
+            break;
+            case 11:
+            System.out.print("thang 12");
+            break;
+          default: System.out.print("khong co thang nay");
+        }
+        
+    }
+}
