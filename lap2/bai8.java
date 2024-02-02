de5af3aeffd366f8bfd13200ac326b41b74337cd
binhduong0508 <binhduong05082004@gmail.com>
2024-02-02 16:56:48 +0700

    bai8lab2

---
+++ lap2/bai8.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class bai8  {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n;
+    do{
+        System.out.print("nhap so phan tu cua mang: ");
+        n =sc.nextInt();
+    } while(n <0);
+    int A[] = new int [n];
+    System.out.println("nhap cac phan tu cho mang");
+    for(int i=1; i< n;i++){
+    System.out.print("nhap phan tu thu i"+i+":");
+    A[i]=sc.nextInt();
+    }
+    double Tong=0;
+    for(int i=1; i<n;i++){
+    Tong+= A[i];
+    }
+    System.out.print("trung binh cong:"+ Tong/n);
+    }
+}
