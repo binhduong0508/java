182a91a1c7c2a66cf691ff7cdd5e3f6ff6171464
binhduong0508 <binhduong05082004@gmail.com>
2024-02-03 00:14:43 +0700

    bai12lab2

---
+++ lap2/bai12.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class bai12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        System.out.print("nhap so cot: ");
+        a=sc.nextInt();
+        System.out.print("nhap so cot: ");
+        b=sc.nextInt();
+        int A[][]= new int[a] [b];
+        System.out.println("nhap cac phan tu cho ma tran");
+        for(int i=0;i<a;i++)
+        for(int j=0;j<a;j++){
+        System.out.println("A["+i+"]["+j+"]=");
+        A[i][j]=sc.nextInt();
+        }
+        int max=A[0][0];
+        for(int i=0;i<a;i++)
+        for(int j=0;j<a;j++){
+            if(A[i][j]>max) 
+            max=A[i][j];
+        }
+        System.out.println("gia tri lon nhat la: "+ max);
+    }
+}
+
